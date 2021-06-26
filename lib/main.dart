2d551ad02e63434d1afcbbc314a9b2e627f4d35e
MOUNIBMarouaneDWM <marouane.mounib@dwm.ma>
2021-06-26 02:01:04 +0100

    jour 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:contacts_messages_app/bloc/contacts.bloc.dart';
+import 'package:contacts_messages_app/repositories/contacts.repo.dart';
+import 'package:contacts_messages_app/ui/pages/contacts/contact.page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +12,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=> ContactsBloc(
+            ContactsState(contacts:[],errorMessage: '',requestState: RequestState.NONE ),
+            new ContactsRepository()))
+      ],
+      child: MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.red),
+        routes: {
+          '/contacts':(context)=>ContactsPage(),
+        },
+        initialRoute: '/contacts',
+      ),
+    );
   }
 }
 
--- lib/ui/pages/contacts/contact.page.dart
+++ lib/ui/pages/contacts/contact.page.dart
@@ -1,14 +1,63 @@
+import 'package:contacts_messages_app/bloc/contacts.bloc.dart';
 import 'package:flutter/material.dart';
-class ContactsPage extends StatelessWidget {
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+class ContactsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('contacts')),
-      body:Center(
-        child: Text('Contacts'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.read<ContactsBloc>().add(LoadAllContactsEvent());
+                    },
+                    child: Text('All contacts'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Students'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Developers'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          BlocBuilder<ContactsBloc, ContactsState>(builder: (context, state) {
+            if (state.requestState == RequestState.LOADING) {
+              CircularProgressIndicator();
+            } else if (state.requestState == RequestState.ERROR) {
+              return Column(
+                children: [
+                  Text('${state.errorMessage}'),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Retry'),
+                  )
+                ],
+              );
+            }
+            else if(state.requestState==RequestState.LOADED){}
+            else{}
+          })
+        ],
       ),
-
     );
   }
 }
